ca2082b15fbb3f206e5ab529b07b927cc5dd7404
Laurentiu Patrascu <lpatrascu55@yahoo.com>
2023-10-26 15:04:25 +0100

    added more details to articleCard to make sorting more visible

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -6,6 +6,10 @@ function ArticleCard({ article }) {
       <Link to={`/articles/${article.article_id}`}>
         <img src={article.article_img_url} alt={article.title} />
         <h1>{article.title}</h1>
+        <h2>
+          Votes:{article.votes} <br /> Comments:{article.comment_count} <br />{" "}
+          Date:{article.created_at}
+        </h2>
       </Link>
     </div>
   );
